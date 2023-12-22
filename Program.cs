219d12f6d47863270f0e4e8664d5348bcabfe3d5
packard_TM <a174126725@gmail.com>
2023-12-22 16:28:18 +0300

    добавил первое задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 1. Совместная работа
+// Напишите программу, которая на вход принимает два целых числа и проверяет, является ли первое число квадратом второго.
+// a = 25,  b = 5     => да
+// a = 2,   b = 10    => нет
+// a = 9,   b = -3    => да
+// a = -3,  b = 9     => нет
+
+Console.Write("Введите первое число: ");
+int.Parse(Console.ReadLine());
